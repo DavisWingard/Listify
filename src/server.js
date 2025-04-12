b0e6ddc81f8f5617a98d899eb9f2e37b6bc2dee4
daviswingard <davisewingard@gmail.com>
2025-04-12 13:57:24 -0700

    added missing user serialization and deserialization

--- src/server.js
+++ src/server.js
@@ -16,6 +16,21 @@ app.use(
   })
 );
 
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
 passport.use(
   new SpotifyStrategy(
     {
