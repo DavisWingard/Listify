75e37ca427ceedccd2ae399c576580bcdfcfb31b
daviswingard <davisewingard@gmail.com>
2025-04-13 20:26:14 -0700

    serialize entire user so passport can grab it for deserialization

--- src/server.js
+++ src/server.js
@@ -16,17 +16,14 @@ app.use(
   })
 );
 
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
-    return cb(null, {
-      id: user.id,
-      username: user.username,
-    });
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
   });
 });
 
-passport.deserializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
     return cb(null, user);
   });
 });
